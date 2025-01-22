2378a457d156e63423064d329b097c1a4b58fc48
zikraariansyah <sailijar88@gmail.com>
2025-01-22 11:35:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,32 +3,95 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Project is62</title>
+    <title>Data Buku</title>
     <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/all.css">
 </head>
 <body>
-<?php 
-    include_once('navbar.php');
-?>
 <div class="container">
     <div class="row mt-5">
-        <div class="col-6 m-auto">
-        <div class="card">
-            <div class="card-header text-center">
-                <h3>Sistem Informasi Mahasiswa</h3>
+        <div class="col-8 m-auto">
+            <div class="card">
+            <div class="card-header">
+                <h3 class="float-start">Data Tabel Buku</h3>
+                <span class="float-end"><a class="btn btn-primary" href="form.php"><i class="fa-solid fa-square-plus"></i> Tambah Data</a></span>
             </div>
-            <div class="card-body text-center">
-                <h4>Selamat Datang </h4>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
-            
+            <div class="card-body">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Judul Buku</th>
+                        <th scope="col">Pengarang</th>
+                        <th scope="col">Kategori</th>
+                        <th scope="col">Tahun Terbit</th>
+                        <th scope="col">Action</th>
+
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    #1. koneksikan file ini
+                    include("koneksi.php");
+
+                    #2. menulis query
+                    $tampil = "SELECT * FROM buku";
+
+                    #3. jalankan query
+                    $proses = mysqli_query($koneksi, $tampil);
+
+                    #4. looping data dari database
+                    $nomor = 1;
+                    foreach($proses as $data){
+                    ?>
+                    <tr>
+                        <th scope="row"><?=$nomor++?></th>
+                        <td><?=$data['judul_buku']?></td>
+                        <td><?=$data['pengarang']?></td>
+                        <td><?=$data['kategori']?></td>
+                        <td><?=$data['tahun_terbit']?></td>
+                        
+
+                        <td>
+                            <a class="btn btn-info btn-sm" href="edit.php"><i class="fa fa-pen-to-square"></i></a>
+                            
+                            <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#hapus<?=$data['id']?>">
+                            <i class="fa-solid fa-trash"></i>
+                            </button>
+
+                            <!-- Modal -->
+                            <div class="modal fade" id="hapus<?=$data['id']?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="exampleModalLabel">Peringatan</h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    Yakin data <b><?=$data['judul_buku']?></b> ingin dihapus?
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+                                    <a href="hapus.php" class="btn btn-danger">Hapus</a>
+                                </div>
+                                </div>
+                            </div>
+                            </div>
+                        </td>
+                    </tr>
+                    <?php
+                    }
+                    ?>
+                </tbody>
+            </table>
             </div>
-        </div>
+            </div>
         </div>
     </div>
 </div>
 
-
     <script src="js/bootstrap.js"></script>
     <script src="js/bootstrap.bundle.js"></script>
+    <script src="js/all.js"></script>
 </body>
 </html>
