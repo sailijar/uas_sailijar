faaa72186edcd7d56f65ea23ece07600e70ee8da
sailijar <sailijar88@gmail.com>
2025-01-22 11:41:24 +0700

    mengubah file index

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Project is62</title>
+    <title>procejt uas</title>
     <link rel="stylesheet" href="css/bootstrap.css">
 </head>
 <body>
