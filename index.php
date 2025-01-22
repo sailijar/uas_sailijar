6164b7aa47177dd7621086dece886b1a64a699e3
zikraariansyah <>
2025-01-22 11:03:21 +0700

    menambah file js dan css

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project is62</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+<body>
+<?php 
+    include_once('navbar.php');
+?>
+<div class="container">
+    <div class="row mt-5">
+        <div class="col-6 m-auto">
+        <div class="card">
+            <div class="card-header text-center">
+                <h3>Sistem Informasi Mahasiswa</h3>
+            </div>
+            <div class="card-body text-center">
+                <h4>Selamat Datang </h4>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
+            
+            </div>
+        </div>
+        </div>
+    </div>
+</div>
+
+
+    <script src="js/bootstrap.js"></script>
+    <script src="js/bootstrap.bundle.js"></script>
+</body>
+</html>
